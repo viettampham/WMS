46691739805010c9fe60921809d55d92f5a50f07
PhamVietTam <phamtampt2k1@gmail.com>
2026-09-25 10:49:26 +0700

    Add gitignore and remove generated files

--- WMS/Models/Request/PagingRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WMS.Models.Request
-{
-    public class PagingRequest
-    {
-        public int PageIndex { get; set; } = 1;   // Trang hiện tại
-        public int PageSize { get; set; } = 10;   // Số bản ghi / trang
-    }
-}
--- WMS/Models/Request/User/AuthenRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WMS.Models.Request.User
-{
-    public class AuthenRequest
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
